d2f0dca4501d5f6edd30215567e2401c01b195bb
MARCELO ADRIAN SUAREZ <90062398+marceloasuarez@users.noreply.github.com>
2023-03-22 08:10:28 -0300

    22/03/2023

---
+++ aula141.js
@@ -0,0 +1,32 @@
+//import { Login } from "./login.js";
+import { Cxmsg } from "./cxmsg.js";
+
+//Login.login("123","321");
+
+const callback_ok=()=>{
+
+	//alert("ok");
+}
+
+const callback_naook=()=>{
+		const config={
+		cor:"#800",
+		tipo:"ok",
+		textos:null,
+		comando_sn:()=>{},
+		
+	}
+	Cxmsg.mostrar(config,"Erro","Login não efetuado! Usuario ou senha incorretos.");
+	//alert("nao ok");
+}
+//Login.login(null,null);
+//Login.login(callback_ok,callback_naook);
+
+const configlogin={
+cor:"048",
+img:"logo.png", 
+endpoint:"https://login.marcelosuarez6.repl.co"
+}
+
+Login.login(callback_ok,callback_naook,configlogin);
+
